f044544d2c0d09ecf3b945642ef5fcffa6c4b655
mazhenjie <mazhenjie@51talk.com>
2018-09-08 10:41:04 +0800

    修改用户组接口,修改登录装饰器

--- ops_billing/create_table.py
+++ ops_billing/create_table.py
@@ -6,8 +6,7 @@ def create_table():
         Asset, Node, Bill, Asset_Node,Service,Asset_Service,Asset_Create_Record,Account,Asset_Account,
         SystemUser,Asset_Create_Template,
         AssetPermission,AssetPerm_Assets,AssetPerm_Nodes,AssetPerm_SystemUser,AssetPerm_Users,AssetPerm_Groups,
-        Groups, User_Group,User,
-        Terminal,Session,Task,Task_Terminal,Session_Terminal,Command,
+        Groups, User_Group,User,UserLoginLog,
         SyncBillInfo,Tasks
     ]
     db.create_tables(create_tables)
