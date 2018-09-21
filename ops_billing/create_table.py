7e4aeed5e31b57979c0a8252c03d2b3a233ec69a
mazhenjie <mazhenjie@51talk.com>
2018-09-21 18:09:09 +0800

    update create_table.py

--- ops_billing/create_table.py
+++ ops_billing/create_table.py
@@ -3,7 +3,7 @@ from app.models import *
 def create_table():
     db.connect()
     create_tables = [
-        Asset, Node, Bill, Asset_Node,Service,Asset_Service,Asset_Create_Record,Account,Asset_Account,
+        Asset, Node, Bill, Asset_Node,Service,Asset_Service,Create_Asset_History,Account,Asset_Account,
         SystemUser,Asset_Create_Template,
         AssetPermission,AssetPerm_Assets,AssetPerm_Nodes,AssetPerm_SystemUser,AssetPerm_Users,AssetPerm_Groups,
         Groups, User_Group,User,UserLoginLog,
